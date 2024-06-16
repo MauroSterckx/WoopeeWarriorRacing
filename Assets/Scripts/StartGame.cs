c4bb40c22d01fda752b86e03d3357871a87287b3
https://gitlab.apstudent.be/enrique.bataire <enrique.bataire@student.ap.be>
2024-06-16 15:36:55 +0200

    Feat: Start Game Logic

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+
+    public List<Material> materials = new List<Material>(); //blue, red, yellow
+
+    public GameObject Car;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string color = GameData.CarColor;
+
+        Renderer renderer = Car.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            if (color.Equals("BLUE"))
+                renderer.material = materials[0];
+            if (color.Equals("RED"))
+                renderer.material = materials[1];
+            if (color.Equals("YELLOW"))
+                renderer.material = materials[2];
+        }
+
+        Pause(5);
+    }
+
+    private IEnumerator Pause(int p)
+    {
+        Time.timeScale = 0.1f;
+        float pauseEndTime = Time.realtimeSinceStartup + 1;
+        while (Time.realtimeSinceStartup < pauseEndTime)
+        {
+            yield return 0;
+        }
+        Time.timeScale = 1;
+    }
+}
+
