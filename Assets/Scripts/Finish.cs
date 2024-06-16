6765acb1cfa24b5250f80d2a43b07c5613444153
https://gitlab.apstudent.be/enrique.bataire <enrique.bataire@student.ap.be>
2024-06-16 22:18:56 +0200

    Update Finish.cs

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -5,18 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
-    // This method is called when the collider attached to this object enters a collision
     private void OnCollisionEnter(Collision collision)
     {
-        // Check if the collided object has the tag "Finish"
         if (collision.gameObject.CompareTag("Finish"))
         {
-            // Load the scene named "endscene"
             SceneManager.LoadScene("endscene");
         }
     }
 
-    // Alternatively, you can use OnTriggerEnter if the wall object has a trigger collider
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Finish"))
